9246b3b8843edfce620a3728daeac6244dc12e3b
FlorinIO <florin.iordache@jordachewd.ro>
2025-07-14 21:17:30 +0300

    Add admin monitoring, security, settings, and updates pages with placeholder content

--- src/app/(admin)/updates/page.tsx
+++ src/app/(admin)/updates/page.tsx
@@ -1,9 +1,9 @@
 import PageHead from "@/components/shared/PageHead";
 
-export default async function AdminDashboard() {
+export default async function AdminUpdates() {
   return (
     <>
-      <PageHead title="Dashboard" alignTitle="left" />
+      <PageHead title="Updates" alignTitle="left" />
 
       <p>
         Lorem ipsum dolor sit amet. Ea itaque natus cum ipsam eveniet aut
@@ -57,83 +57,7 @@ export default async function AdminDashboard() {
         obcaecati cum velit saepe qui dolores cupiditate hic blanditiis
         similique.
       </p>
-      <br />
-      <p>
-        Sed dolor libero sit omnis veniam sed repellat omnis eos nisi temporibus
-        est laudantium internos. Hic voluptatem cumque ad odio impedit et
-        quisquam accusamus. Ut eveniet officia id officia impedit qui
-        consequatur veritatis quo laboriosam sequi et rerum quibusdam ea
-        accusamus molestiae quo explicabo ducimus. Lorem ipsum dolor sit amet.
-        Ea itaque natus cum ipsam eveniet aut blanditiis quis 33 illum eaque a
-        voluptatem cupiditate et excepturi aperiam ea perferendis iure. Sit
-        deleniti iusto et galisum modi eos repellendus officiis et enim
-        deserunt. Qui voluptate optio ab iusto sint est inventore officiis. Et
-        enim mollitia sed ducimus consequatur qui veniam modi vel amet modi! Aut
-        consequuntur omnis id accusantium obcaecati cum velit saepe qui dolores
-        cupiditate hic blanditiis similique. Sed dolor libero sit omnis veniam
-        sed repellat omnis eos nisi temporibus est laudantium internos. Hic
-        voluptatem cumque ad odio impedit et quisquam accusamus. Ut eveniet
-        officia id officia impedit qui consequatur veritatis quo laboriosam
-        sequi et rerum quibusdam ea accusamus molestiae quo explicabo ducimus.
-        Lorem ipsum dolor sit amet. Ea itaque natus cum ipsam eveniet aut
-        blanditiis quis 33 illum eaque a voluptatem cupiditate et excepturi
-        aperiam ea perferendis iure. Sit deleniti iusto et galisum modi eos
-        repellendus officiis et enim deserunt.
-      </p>
-      <br />
-      <p>
-        Qui voluptate optio ab iusto sint est inventore officiis. Et enim
-        mollitia sed ducimus consequatur qui veniam modi vel amet modi! Aut
-        consequuntur omnis id accusantium obcaecati cum velit saepe qui dolores
-        cupiditate hic blanditiis similique. Sed dolor libero sit omnis veniam
-        sed repellat omnis eos nisi temporibus est laudantium internos. Hic
-        voluptatem cumque ad odio impedit et quisquam accusamus. Ut eveniet
-        officia id officia impedit qui consequatur veritatis quo laboriosam
-        sequi et rerum quibusdam ea accusamus molestiae quo explicabo ducimus.
-        Lorem ipsum dolor sit amet. Ea itaque natus cum ipsam eveniet aut
-        blanditiis quis 33 illum eaque a voluptatem cupiditate et excepturi
-        aperiam ea perferendis iure. Sit deleniti iusto et galisum modi eos
-        repellendus officiis et enim deserunt. Qui voluptate optio ab iusto sint
-        est inventore officiis. Et enim mollitia sed ducimus consequatur qui
-        veniam modi vel amet modi! Aut consequuntur omnis id accusantium
-        obcaecati cum velit saepe qui dolores cupiditate hic blanditiis
-        similique. Sed dolor libero sit omnis veniam sed repellat omnis eos nisi
-        temporibus est laudantium internos. Hic voluptatem cumque ad odio
-        impedit et quisquam accusamus. Ut eveniet officia id officia impedit qui
-        consequatur veritatis quo laboriosam sequi et rerum quibusdam ea
-        accusamus molestiae quo explicabo ducimus. Lorem ipsum dolor sit amet.
-        Ea itaque natus cum ipsam eveniet aut blanditiis quis 33 illum eaque a
-        voluptatem cupiditate et excepturi aperiam ea perferendis iure. Sit
-        deleniti iusto et galisum modi eos repellendus officiis et enim
-        deserunt. Qui voluptate optio ab iusto sint est inventore officiis. Et
-        enim mollitia sed ducimus consequatur qui veniam modi vel amet modi! Aut
-        consequuntur omnis id accusantium obcaecati cum velit saepe qui dolores
-        cupiditate hic blanditiis similique.
-      </p>
-      <br />
-      <p>
-        Sed dolor libero sit omnis veniam sed repellat omnis eos nisi temporibus
-        est laudantium internos. Hic voluptatem cumque ad odio impedit et
-        quisquam accusamus. Ut eveniet officia id officia impedit qui
-        consequatur veritatis quo laboriosam sequi et rerum quibusdam ea
-        accusamus molestiae quo explicabo ducimus.
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet. Ea itaque natus cum ipsam eveniet aut
-        blanditiis quis 33 illum eaque a voluptatem cupiditate et excepturi
-        aperiam ea perferendis iure. Sit deleniti iusto et galisum modi eos
-        repellendus officiis et enim deserunt. Qui voluptate optio ab iusto sint
-        est inventore officiis. Et enim mollitia sed ducimus consequatur qui
-        veniam modi vel amet modi! Aut consequuntur omnis id accusantium
-        obcaecati cum velit saepe qui dolores cupiditate hic blanditiis
-        similique. Sed dolor libero sit omnis veniam sed repellat omnis eos nisi
-        temporibus est laudantium internos. Hic voluptatem cumque ad odio
-        impedit et quisquam accusamus. Ut eveniet officia id officia impedit qui
-        consequatur veritatis quo laboriosam sequi et rerum quibusdam ea
-        accusamus molestiae quo explicabo ducimus.
-      </p>
-      <br />
+
       <p>
         Lorem ipsum dolor sit amet. Ea itaque natus cum ipsam eveniet aut
         blanditiis quis 33 illum eaque a voluptatem cupiditate et excepturi
--- src/components/shared/PageHead.tsx
+++ src/components/shared/PageHead.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import css from "@/styles/shared/PageHead.module.css";
+import { useAuth } from "@clerk/nextjs";
 import { Typography } from "@mui/material";
+import { useEffect } from "react";
 
 interface PageHeadProps {
   title: string;
@@ -9,6 +13,7 @@ interface PageHeadProps {
   size?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   children?: React.ReactNode;
 }
+
 export default function PageHead({
   title,
   subtitle,
@@ -17,6 +22,23 @@ export default function PageHead({
   alignTitle = "center",
   alignSubtitle = "center",
 }: PageHeadProps) {
+  /** For development purpose - To Be Removed  */
+  const { getToken, isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    console.log("PageHead - isLoaded:", isLoaded, "isSignedIn:", isSignedIn);
+    const logToken = async () => {
+      if (isLoaded && isSignedIn) {
+        const token = await getToken();
+        console.log("--- Clerk Session JWT (Copy this for Swagger UI) ---");
+        console.log(token);
+        console.log("--------------------------------------------------");
+      }
+    };
+    logToken();
+  }, [isLoaded, isSignedIn, getToken]);
+  /* END PageHead */
+
   return (
     <div className={css.section}>
       <div className={css.title}>
